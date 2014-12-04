976c63f9ae3c5318277127adeb74bbeb09854814
parlex <premer@outlook.com>
2014-12-04 02:40:05 +0100

    removed file

--- gitToJson.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import os;
-
-os.system("git log     --pretty=format:'{%n  "commit": "%H",%n  "author": "%an <%ae>",%n  "date": "%ad",%n  "message": "%f"%n},'     $@ |     perl -pe 'BEGIN{print "["}; END{print "]\n"}' |     perl -pe 's/},]/}]/' > gitlog.json
-");
