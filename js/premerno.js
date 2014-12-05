2dc3fd156076c359193c5235f33cf73c5854eb3d
parlex <premer@outlook.com>
2014-12-05 01:16:58 +0100

    Added funny text js and css and applied it to the page title

--- js/premerno.js
+++ js/premerno.js
@@ -3,6 +3,18 @@
  */
 var app = angular.module('premerApp', []);
 
+app.controller("MainController", ['$scope', function($scope, $http)
+    {
+        angular.element(document).ready(function () {
+            $('.mySelector').funnyText({
+                borderColor: 'none',
+                activeColor: 'orange',
+                direction: 'both'
+            });
+        });
+    }]
+);
+
 app.controller("JsonReader", ['$scope','$http', function($scope, $http)
     {
         $http.get('gitlog.json').success (function(data){
